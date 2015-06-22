609d3ba4abdf0ba0c3425f48873db15e69b10ebf
frank <frankchen@fashionguide.com.tw>
2015-06-22 12:05:06 +0800

    fb分享

--- fb_webroot/public/js/main.js
+++ fb_webroot/public/js/main.js
@@ -2,9 +2,9 @@ $(function () {
 
   var _app_id = '998967653470510';
   var _api_key = '';
-
+  var fb_id;
   //驗證
-  window.fbAsyncInit = function() {
+  window.fbAsyncInit = function () {
     FB.init({
       appId: _app_id,
       display: 'dialog',
@@ -12,15 +12,18 @@ $(function () {
       cookie: true, // enable cookies to allow the server to access the session
       xfbml: true,  // parse XFBML
       oauth: true,  // enable OAuth 2.0
-      version : 'v2.3'
+      version: 'v2.3'
     });
     FB.Canvas.setAutoGrow(); //autoResize  → no scrollbar
   };
 
-  (function(d, s, id){
+  (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
     js.src = "//connect.facebook.net/zh_TW/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
@@ -35,9 +38,9 @@ $(function () {
     FB.getLoginStatus(function (response) {
       if (response.authResponse) {
 
-        var u_fb_id = response.authResponse.userID;
+        fb_id = response.authResponse.userID;
         var accessToken = response.authResponse.accessToken;
-
+        console.log(fb_id);
 
         share();
         //openGraphStories();
@@ -53,37 +56,47 @@ $(function () {
   function login() {
     FB.login(function (response) {
       if (response.authResponse) {
-        var u_fb_id = response.authResponse.userID;
         console.log('FB.login response', response);
+        getLoaginState();
       } else {
         alert('須同意應用程式');
       }
     });
+
   }
 
   function feed() {
     FB.ui({
+      display: 'popup',
       method: 'feed',
       link: 'http://fashionguide.com.tw',
       caption: 'An example caption',
-      message :'ppppp'
-    }, function(response){
-      console.log(response);
+      description: 'A small JavaScript library that allows you to harness ' +
+                   'the power of Facebook, bringing the user\'s identity, ' +
+                   'social graph and distribution power to your site.',
+      picture: 'http://i.fgi.tw/Beauty/ImgPro/1/62612s.jpg'
+    }, function (response) {
+      if (response && !response.error_code) {
+        console.log("OK: " + JSON.stringify(response));
+        console.log('ready to submit: ' + fb_id);
+      } else {
+        console.log("Not OK: " + JSON.stringify(response));
+      }
     });
   }
 
   function share() {
     FB.ui({
       //method: 'share',
-      //href: 'https://tw.yahoo.com/'
       display: 'popup',
       method: 'share',
-      href: 'http://www.fashionguide.com.tw/'
-    }, function(response){
+      href: 'http://yahoo.com.tw'
+    }, function (response) {
       if (response && !response.error_code) {
-        console.log("OK: "+JSON.stringify(response));
+        console.log("OK: " + JSON.stringify(response));
+        console.log('ready to submit: ' + fb_id);
       } else {
-        console.log("Not OK: "+JSON.stringify(response));
+        console.log("Not OK: " + JSON.stringify(response));
       }
     });
   }
@@ -94,9 +107,9 @@ $(function () {
       method: 'share_open_graph',
       action_type: 'og.likes',
       action_properties: JSON.stringify({
-        object:'https://developers.facebook.com/docs/'
+        object: 'https://developers.facebook.com/docs/'
       })
-    }, function(response){
+    }, function (response) {
       console.log(response);
     });
   }
@@ -108,14 +121,10 @@ $(function () {
     //
     //});
 
-    FB.api('me?fields=id,name,picture', function(response){
+    FB.api('me?fields=id,name,picture', function (response) {
       console.log(response);
     });
   }
-
-
-
-
 
 
 });
